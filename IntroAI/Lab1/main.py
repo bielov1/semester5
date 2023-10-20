ab3df4192641ac6532329903543b98c0984d7229
Oleh Bielov <olaph22@gmail.com>
2023-10-20 23:55:55 +0300

    finished lab1

--- IntroAI/Lab1/main.py
+++ IntroAI/Lab1/main.py
@@ -1,5 +1,15 @@
+# Вступ до штучного інтелекту. ЛР1
+#
+# Розробити алгоритм генерації дороги у вигляді графу.
+# Зі згенерованого графу видалити певну кількість ребер так,
+# щоб результуючий граф залишився повністю зв'язним.
+#
+# Бєлов Олег, IO-11, 20.10.2023
+
+
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
 
 all_nodes = []
 
@@ -13,7 +23,20 @@ def neightbors(node):
 
 	return result
 
-def generate_graph(n):
+def is_connected(graph):
+	visited = set()
+	stack = [list(graph.nodes)[0]]
+
+	while stack:
+		node = stack.pop()
+		if node not in visited:
+			visited.add(node)
+			stack.extend([n for n in graph.neighbors(node) if n not in visited])
+
+	return len(visited) == len(graph.nodes)
+
+
+def generate_graph(n, ke):
 	for x in range(n):
 		for y in range(n):
 			all_nodes.append((x, y))
@@ -24,13 +47,27 @@ def generate_graph(n):
 		for neigh in neightbors(node):
 			G.add_edge(node, neigh)
 
-
-	pos = {node: (node[0], node[-1]) for node in G.nodes()}
-	nx.draw(G, pos, with_labels=True, node_size=300, node_color='grey', width = 5, edge_color='pink')
-
+	# removing fixed number of edges so the graph keeps being connected
+	edges_removed = 0
 	edges = list(G.edges)
+	random.shuffle(edges)
+	for edge in edges:
+		if edges_removed >= ke:
+			break
+		
+		G.remove_edge(*edge)
+		if not is_connected(G):
+			G.add_edge(*edge)
+		else:
+			edges_removed += 1
+
+
+		
+	pos = {node: (node[0], node[-1]) for node in G.nodes()}
+	nx.draw(G, pos, with_labels=False, node_size=300, node_color='grey', width = 10, edge_color='orange')
 	plt.show()
 
 if __name__ == '__main__':
 	n = int(input("Enter n: "))
-	generate_graph(n)
+	kill_e = int(input("Enter number of edges to remove: "))
+	generate_graph(n, kill_e)
